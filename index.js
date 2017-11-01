dabe4fd4ccbee5a0366a316c86bb1d800458044d
Aditya Desai <adityadesai97@gmail.com>
2017-11-01 23:20:51 +0530

    changes

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ function setAnims() {
     card.addEventListener('mouseover', (e) => {
       card.children[1].style.bottom = `-${card.offsetHeight}px`;
       card.children[0].style.left = '10px'
-      card.parentElement.children[0].classList.add('zoom-img');
+      // card.parentElement.children[0].classList.add('zoom-img');
       // card.style.background = 'linear-gradient(transparent 0%, #191919)';
     });
     card.addEventListener('mouseout', (e) => {
       card.children[1].style.bottom = '10px';
       card.children[0].style.left = `-${card.children[0].offsetWidth + 50}px`;
-      card.parentElement.children[0].classList.remove('zoom-img');
+      // card.parentElement.children[0].classList.remove('zoom-img');
       // card.style.background = 'linear-gradient(transparent 50%, #191919)';
     });
   });
@@ -21,7 +21,13 @@ function setAnims() {
 
 $('.parallax').css('height', $(window).height());
 
-$(document).on('click', 'a[href^="#"]', function (event) {
+$(window).on('scroll', (event) => {
+  document.getElementById('scroll_down_arrow').style.height = 0;
+  document.getElementById('scroll_down_text').style.height = 0;
+  document.getElementById('scroll_down_text').innerHTML = "";
+});
+
+$(document).on('click', 'a[href^="#"].main-navbar', function (event) {
     event.preventDefault();
 
     $('html, body').animate({
